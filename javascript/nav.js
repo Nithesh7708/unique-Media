13998b11e6ac6af94ed7ecfe5b51aea689cc264f
Nithesh kumar <117974041+Nithesh7708@users.noreply.github.com>
2024-08-07 09:47:52 +0530

    home animation

--- javascript/nav.js
+++ javascript/nav.js
@@ -6,4 +6,10 @@ menu_btn.addEventListener('click',()=>{
     checkbox.checked==true?body.classList.remove("scroll_hidden"):body.classList.add("scroll_hidden");
 })
 
-
+// Mouse animation
+var pos =document.documentElement;
+pos.addEventListener('mousemove',(e)=>{
+   let x= pos.style.setProperty('--x',e.clientX+'px')
+  let y=  pos.style.setProperty('--y',e.clientY+'px')
+  
+})

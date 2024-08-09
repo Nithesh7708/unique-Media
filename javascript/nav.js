c46a0a8bd3870de58eece139a0e92372fc0ce1b6
Nithesh kumar <117974041+Nithesh7708@users.noreply.github.com>
2024-08-09 08:05:19 +0530

    i add home page with response

--- javascript/nav.js
+++ javascript/nav.js
@@ -13,3 +13,12 @@ pos.addEventListener('mousemove',(e)=>{
   let y=  pos.style.setProperty('--y',e.clientY+'px')
   
 })
+
+// --------Menu-List---------------
+let menu_icon=document.querySelector(".menu label");
+let menu_checkbox=document.getElementById("menubar");
+let menu_list=document.getElementById("menu_list_checkbox")
+
+menu_icon.addEventListener('click',()=>{
+menu_checkbox.checked==true?menu_list.checked=false:menu_list.checked=true;
+})
